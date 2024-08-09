d64b8cfe534011733a5a311b25b3b438e7c58803
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-08-09 22:18:06 +0400

    implement header

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      <Button variant="outline">Button</Button>
-      <h2>Hello</h2>
-    </div>
-  );
+  return <div>Home</div>;
 }
